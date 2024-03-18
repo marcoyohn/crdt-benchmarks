c7b1f2e0a3fcc49b9fded82806aaa460b2c89ac6
杨茂聪 <yangmaocong@cvte.com>
2024-03-18 10:49:33 +0800

    +

--- js-lib/collab_cpu_doc_update.js
+++ js-lib/collab_cpu_doc_update.js
@@ -22,7 +22,7 @@ export const runBenchmarksCollabCpuDocUpdate = async (crdtFactory, filter) => {
     // https://hub-she.seewo.com/she-engine-res-hub/wopi/files/133687528128513/133687532322817
     const doc = crdtFactory.create((update, local) => {
       docUpdateSize = docUpdateSize + update.length
-    }, true, 'ws://172.30.64.132:1234/ws/', docName + ".cowork")
+    }, true, 'ws://172.30.109.16:1234/ws/', docName + ".cowork")
     
     doc.transact( () => {
       for (let i = 0; i < inputData.length; i++) {
